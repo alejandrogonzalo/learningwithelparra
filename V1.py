a68c97d87782ec94b31b1ddf852cb23b67c780b3
alejandrogonzalo <47109888+alejandrogonzalo@users.noreply.github.com>
2021-06-21 17:43:08 +0200

    AGM - Add V1.py

---
+++ V1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+from PIL import Image
+import requests
+from datetime import datetime
+import requests
+import telegram
+
+
+# Descarga la imagen a petición y la guarda en el local:
+
+# In[10]:
+
+
+def save_image():
+    datatime = datetime.today().strftime('%Y-%m-%d')
+    path = (r"C:\Users\alejandro\Desktop\Bot_telegram\im_" + datatime + ".jpg")
+
+    url = "https://www.comunidad.madrid/sites/default/files/styles/imagen_enlace_opcional/public/doc/sanidad/info/niveles_de_reserva_sangre.jpg"
+
+    response = requests.get(url, stream=True)
+    if response.ok and (response.status_code == 200):
+        im = Image.open(response.raw)
+        im.save(path)
+    else:
+        print(f"Request is fail with error {response.status_code}. Please check url {url}")
+
+
+# Recorre los chats y envía la foto a cada uno sin repetirse
+
+# In[13]:
+
+
+def send_image():
+    bot_token = '1860902556:AAFh53pvK5-cugsRmjRbSOnZsZFZ-xU-4GQ'
+    bot_chatID = 'bot_chatID'
+    bot = telegram.Bot(bot_token)
+    response = requests.get(f"https://api.telegram.org/bot{bot_token}/getUpdates")
+
+    if response.ok and (response.status_code ==200):
+        response_chat = response.json()["result"]
+        bot_chatID_list =[]
+
+        for i in response_chat:
+            bot_chatID = i["message"]["from"]["id"]
+            bot_chatID_str = str(bot_chatID)
+    
+            if (bot_chatID_str in bot_chatID_list) == False:
+            bot.sendPhoto(bot_chatID_str,url)
+            bot_chatID_list.append(bot_chatID_str)
+    else:
+        print(f"Request is fail with error {response.status_code}. Please check url https://api.telegram.org/bot{bot_token}/getUpdates")
+
+
+# In[ ]:
+
+
+save_image()
+send_image()
+
