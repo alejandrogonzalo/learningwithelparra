08697d531b51d311afb786ebb78b219ee840b924
alejandrogonzalo <alejandrogonzalom@gmail.com>
2021-06-29 15:34:36 +0200

    joderlo todo porque sí

--- V1.py
+++ V1.py
@@ -9,26 +9,8 @@ import telegram
 
 
 # Descarga la imagen a petición y la guarda en el local:
+POLLAS POLLAS POLLAS
 
-def save_image():
-    datatime = datetime.today().strftime('%Y-%m-%d')
-    path = (r"C:\Users\alejandro\Desktop\Bot_telegram\im_" + datatime + ".jpg")
-
-    url = "https://www.comunidad.madrid/sites/default/files/styles/imagen_enlace_opcional/public/doc/sanidad/info/niveles_de_reserva_sangre.jpg"
-
-    response = requests.get(url, stream=True)
-    if response.ok and (response.status_code == 200):
-        im = Image.open(response.raw)
-        im.save(path)
-    else:
-        print(f"Request is fail with error {response.status_code}. Please check url {url}")
-
-
-# Recorre los chats y envía la foto a cada uno sin repetirse
-
-def send_image():
-    bot_token = '1860902556:AAFh53pvK5-cugsRmjRbSOnZsZFZ-xU-4GQ'
-    bot_chatID = 'bot_chatID'
     bot = telegram.Bot(bot_token)
     response = requests.get(f"https://api.telegram.org/bot{bot_token}/getUpdates")
 
